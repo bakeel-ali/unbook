8d1933feaa7e1b883d1ff8a0bd9240ba75ac5dfd
bakeel-ali <bakeelaledrisy@gmail.com>
2024-12-24 00:51:56 +0300

    Update translationFile.js

--- translationFile.js
+++ translationFile.js
@@ -1,8 +1,8 @@
 // const translations = {
 //     en: {
 //         txt1 : "this project designing by",
-//         txt2 : "Khabab Al_Shahery",
-//         txt3 : "Bakeel AL_Edreesy",
+//         txt2 : "Bakeel Al_Edrisy",
+//         txt3 : "Khabab AL_Shahery",
 //         txt4 : "Ragi Al_Sarawi",
 //         txt5 : "Home",
 //         txt6 : "Section",
@@ -10,8 +10,8 @@
 //     },
 //     er: {
 //         txt1 : "تم تصميم هذا المشروع بواسطة",
-//         txt2 : "خباب الشاهري",
-//         txt3 : "بكيل الادريسي",
+//         txt2 : "بكيل الادريسي",
+//         txt3 : "خباب الشاهري",
 //         txt4 : "راجي الساروي",
 //         txt5 : "الرئيسية",
 //         txt6 : "قسم",
